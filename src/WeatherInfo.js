737564e7aef193dbba290a854a1825edfe5012e8
Marvinrose <rozzeymarvin32@gmail.com>
2022-09-17 04:38:28 +0100

    Edited weatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
-export default function weatherInfo(props) {
+export default function WeatherInfo(props) {
   return (
     <div className="weatherInfo">
       <div className="row">
